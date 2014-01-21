80108ff6def9d8c902a1bd6d3b5e4f9bf4f7fb5c
barbozamatias <barbozamatias@gmail.com>
2014-01-21 16:48:30 -0200

    Update funciones_utiles.php

--- funciones_utiles.php
+++ funciones_utiles.php
@@ -1,3 +1,11 @@
 //Renombrar un archivo o directorio
 
 rename("/tmp/archivo_tmp.txt", "/home/user/login/docs/mi_archivo.txt");
+
+//Guarda en el archivo indicado por un path el contenido $_content
+
+function guardar_archivo($archivo, $_content){
+		$abrir = fopen($archivo,'w');
+		fwrite($abrir,$_content);
+		fclose($abrir);
+}
